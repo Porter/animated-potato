e6a4ba5209b62896bf22e879f71984098710606e
Porter <pmh192@gmail.com>
2024-01-31 23:01:54 -0800

    starting over

--- init.lua
+++ init.lua
@@ -1,100 +1,74 @@
-local dirs = {'h', 'j', 'k', 'l'}
+-- Options
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.incsearch = true
+vim.o.hlsearch = false
+vim.o.showcmd = true
+vim.o.mouse = ""
 
-vim.api.nvim_set_keymap('n', '<space>', '<nop>', {noremap = true})
+-- Getting around
+vim.keymap.set('n', '<space>', '<nop>', {noremap = true})
 vim.g.mapleader = ' '
 
-vim.api.nvim_set_keymap('n', '<leader>ev', ':vsplit ~/.config/nvim/init.lua<cr>', {noremap = true})
+-- Editing neovim
+vim.keymap.set('n', '<leader>ev', '<cmd>e ~/.config/nvim/init.lua<cr>', {noremap = true})
+vim.keymap.set('n', '<leader>sv', '<cmd>source ~/.config/nvim/init.lua<cr>', {noremap = true})
 
--- Use control+dir to move between windows.
-for _, dir in ipairs(dirs) do
-	vim.api.nvim_set_keymap('n', '<c-' .. dir .. '>', '<c-w>' .. dir, {noremap = true})
-end
+-- Insert mode
+vim.keymap.set('i', 'jk', '<esc>', {noremap = true})
+vim.keymap.set('t', 'jk', '<c-\\><c-n>', {noremap = true})
+vim.keymap.set('i', 'jl', '<esc><cmd>w<cr>', {noremap = true})
 
-
--- Leave insert mode quickly.
-vim.api.nvim_set_keymap('i', 'jk', '<esc>', {noremap = true})
-vim.api.nvim_set_keymap('i', 'jl', '<esc>:w<cr>', {noremap = true})
-
--- Tabs.
-vim.api.nvim_set_keymap('n', '<c-n>', ':tabnext<cr>', {noremap = true})
-
-function findTerminalBuffer()
-	for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-		if vim.api.nvim_buf_is_valid(buf) then
-			success, channel = pcall(function() return vim.bo[buf].channel end)
-			if success and channel ~= 0 then
-				return buf
-			end
-		end
-	end
-end
-
-function findOrCreateTerm()
-	-- Look for a tab that has a window with an open buffer that has channel option set.
-	for _, tab in ipairs(vim.api.nvim_list_tabpages()) do
-		for _, win in ipairs(vim.api.nvim_tabpage_list_wins(tab)) do
-			local buf = vim.api.nvim_win_get_buf(win)
-			success, channel = pcall(function() return vim.bo[buf].channel end)
-			if success and channel ~= 0 then
-				return tab, buf, channel
-			end
-		end
-	end
-
-	-- Look at the last tab a terminal command was run. Switch to the terminal if there is only
-	-- one window open.
-	local tab = _G.lastTabWithTermHandle
-	local buf = _G.lastTerminalBuffer
-	if tab ~= nil and vim.api.nvim_tabpage_is_valid(tab) then
-		local winCount = 0
-		local win = nil
-		for _, w in ipairs(vim.api.nvim_tabpage_list_wins(tab)) do
-			winCount = winCount + 1
-			win = w
-		end
-
-		success, channel = pcall(function() return vim.bo[buf].channel end)
-		if winCount == 1 and success and channel ~= 0 then
-			vim.api.nvim_exec("normal " .. tab .. "gtG", true) -- go to the tab.
-			vim.api.nvim_win_set_buf(win, buf) -- set the terminal buffer
-			return tab, buf, channel
-		end
-	end	
-
-
-	-- Create a new tab, and start a terminal if neccessary.
-	vim.api.nvim_exec("tabnew", true) -- create a new tab.
-	buf = findTerminalBuffer()
-	if buf == nil then
-		vim.api.nvim_exec("terminal", true) -- start a new terminal.
-		buf = vim.api.nvim_win_get_buf(0)
-	end
+-- Tabs
+vim.keymap.set('n', '<m-l>', '<cmd>tabnext<cr>', {noremap = true})
+vim.keymap.set('n', '<m-h>', '<cmd>tabprev<cr>', {noremap = true})
+vim.keymap.set('n', '<leader>tn', '<cmd>tabnew<cr>', {noremap = true})
+local function duplicateTab()
+	local buf = vim.api.nvim_buf_get_number(0)
+	vim.cmd("tabnew")
 	vim.api.nvim_win_set_buf(0, buf)
-	success, channel = pcall(function() return vim.bo[buf].channel end)
-	if not success then
-		print("Unable to start a new terminal")
-		return
+end
+vim.keymap.set('n', '<leader>td', '<cmd>lua idkyet.duplicateTab()<cr>', {noremap = true})
+
+-- Blankspace
+vim.keymap.set('n', '<leader>sws', '<cmd>set listchars=space:_,tab:>~ list<cr>', {noremap = true})
+vim.keymap.set('n', '<leader>hws', '<cmd>set listchars=eol:$ nolist<cr>', {noremap = true})
+
+-- Plugins
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local function tsInstall()
+	local langs = { "go" }
+	for i, lang in ipairs(langs) do
+		vim.cmd("TSInstall " .. lang)
 	end
-	return vim.api.nvim_win_get_tabpage(0), buf, channel
 end
 
-function termRun(cmd)
-	tab, buf, channel = findOrCreateTerm()
-	if channel ~= 0 then
-		vim.fn.chansend(channel, cmd .. '\n')
-		local num = vim.api.nvim_tabpage_get_number(tab)
-		vim.api.nvim_exec("normal " .. num .. "gtG", true) -- go to tab #num.
 
-		_G.lastTabWithTermHandle = tab
-		_G.lastTerminalBuffer = buf
-	end
-end
+idkyet = {
+	duplicateTab = duplicateTab,
+	tsInstall = tsInstall,
+}
 
-vim.api.nvim_create_user_command("Make", [[lua termRun("cargo build")]], {})
-vim.api.nvim_set_keymap('n', '<leader>bb', ':Make<cr>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<leader><Up>', ':lua termRun("!!")<cr>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<leader>br', ':lua termRun("cargo run")<cr>', {noremap = true})
+require("lazy").setup({
+	{ "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = ...},
+	{ "nvim-treesitter/nvim-treesitter" },
+	{ "L3MON4D3/LuaSnip" },
+	{ "hrsh7th/nvim-cmp" },
+	{ "nvim-telescope/telescope.nvim", dependencies = { 'nvim-lua/plenary.nvim' } },
+})
 
--- copy to clipboard
-vim.api.nvim_set_keymap('v', '<leader>y', [["*y:call system("echo -n " . shellescape(getreg("*")) . " | xclip -i -selection clipboard")<cr>]], {noremap = true})
-vim.api.nvim_set_keymap('n', '<leader>fy', [[:call system("echo -n " . shellescape(getreg("%")) . " | xclip -i -selection clipboard")<cr>]], {noremap = true})
+-- Color
+
+vim.cmd([[colorscheme gruvbox]])
